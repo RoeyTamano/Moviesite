6b7f3374605b6bbd332560bf26c629c5a4834118
RoeyTamano <135346359+RoeyTamano@users.noreply.github.com>
2024-05-13 15:46:47 +0300

    Add files via upload

---
+++ web.js
@@ -0,0 +1,27 @@
+function showText1() {
+    document.getElementById('output1').textContent = 'Spiderman is a superhero that spider bite him, and he got a superpower';
+}
+function hideText1() {
+    document.getElementById('output1').textContent = '';
+}
+//2
+function showText2() {
+    document.getElementById('output2').textContent = 'StarWars is a si-fi movie that hapening in space';
+}
+function hideText2() {
+    document.getElementById('output2').textContent = '';
+}
+//3
+function showText3() {
+    document.getElementById('output3').textContent = 'Dragon ball is an Anime series with a lot of fights';
+}
+function hideText3() {
+    document.getElementById('output3').textContent = '';
+}
+//4
+function showText4() {
+    document.getElementById('output4').textContent = 'Avangers is a movie of superhero that save the world from a big threat';
+}
+function hideText4() {
+    document.getElementById('output4').textContent = '';
+}
